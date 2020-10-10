618c62b04c58ef26cf875c53ae874c6a9e59ac33
Aquil Hassan <acquil98@gmail.com>
2020-10-10 14:15:50 +0000

    Basic crawling - questions and urls

--- stack/stack/items.py
+++ stack/stack/items.py
@@ -8,6 +8,8 @@ from scrapy.item import Item, Field
 
 class StackItem(scrapy.Item):
     # define the fields for your item here like:
-    name = scrapy.Field()
-    # pass
-    
+    # name = scrapy.Field()
+    #pass
+    title = Field()
+    url = Field()
+
