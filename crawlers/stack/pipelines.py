9acfc286afa5610dcd88748ba26adda55c8b120a
Aquil Hassan <acquil98@gmail.com>
2020-10-11 13:36:53 +0000

    Added pipelines for mongo and removing duplicates

--- crawlers/stack/pipelines.py
+++ crawlers/stack/pipelines.py
@@ -6,8 +6,10 @@
 
 import json
 
+import pymongo
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
 
 
 class StackPipeline:
@@ -27,4 +29,44 @@ class JsonWriterPipeline:
     def process_item(self, item, spider):
         line = json.dumps(ItemAdapter(item).asdict()) + "\n"
         self.file.write(line)
+        return item
+
+
+class DuplicatesPipeline:
+
+    def __init__(self):
+        self.ids_seen = set()
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        if adapter['id'] in self.ids_seen:
+            raise DropItem("Duplicate item found: %r" % item)
+        else:
+            self.ids_seen.add(adapter['id'])
+            return item
+
+
+class MongoPipeline:
+    collection_name = 'scrapy_items'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        self.db[self.collection_name].insert_one(ItemAdapter(item).asdict())
         return item
