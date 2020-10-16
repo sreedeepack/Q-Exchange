9d8c897053d81c2a2a0a33a3e8d5efb39317c08d
Aquil Hassan <acquil98@gmail.com>
2020-10-16 22:23:18 +0000

    Download delay: Experiment #9

--- crawlers/stack/settings.py
+++ crawlers/stack/settings.py
@@ -30,7 +30,7 @@ ROBOTSTXT_OBEY = False
 DOWNLOAD_DELAY = 5
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 8
-# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 2
 # CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
