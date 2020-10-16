d4f79d9573c1946f5605842568b0a2abfe0df934
Aquil Hassan <acquil98@gmail.com>
2020-10-16 22:29:23 +0000

    Download delay: Experiment #10

--- crawlers/stack/settings.py
+++ crawlers/stack/settings.py
@@ -16,10 +16,10 @@ NEWSPIDER_MODULE = 'stack.spiders'
 STACK_PAGE_LIMIT = 50
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'stack-crawler'
+USER_AGENT = 'stack'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
@@ -27,14 +27,14 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 5
+DOWNLOAD_DELAY = 10
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 8
-CONCURRENT_REQUESTS_PER_DOMAIN = 2
+# CONCURRENT_REQUESTS_PER_DOMAIN = 2
 # CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-COOKIES_ENABLED = False
+COOKIES_ENABLED = True
 
 # Disable Telnet Console (enabled by default)
 # TELNETCONSOLE_ENABLED = False
